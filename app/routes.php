e3bfad8b957a7f87eb8e3d9053bf01e95f850e39
Justin Page <xjustinpagex@gmail.com>
2014-03-07 23:44:58 -0800

    trying out queue

--- app/routes.php
+++ app/routes.php
@@ -15,9 +15,13 @@ Route::get('/', array('as' => 'new_snippet', 'uses' => 'SnippetsController@creat
 Route::get('{num}', array('as' => 'snippet', 'uses' => 'SnippetsController@show'));
 Route::get('{num}/fork', array('as' => 'fork_snippet', 'uses' => 'SnippetsController@fork'));
 Route::post('/', array('uses' => 'SnippetsController@store'));
-Route::get('/greeting', function()
+
+
+// Queue for iron.io
+Route::post('queue', function()
 {
-	dd('hello, justin');
+	Log::info('marshal!');
+	return Queue::marshal();
 });
 
 
