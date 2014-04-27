03a059ea50760e804b5b995d3b5f548f4d69dda1
Justin Page <xjustinpagex@gmail.com>
2014-04-26 17:09:00 -0700

    reset back to share snippets'

--- app/routes.php
+++ app/routes.php
@@ -9,33 +9,36 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
 |
-*/
+ */
 
 
-// Route::get('users/store', 'UsersController@store');
-// Route::resource('users', 'UsersController');
-Route::get('/', function()
-{
-	Queue::push('FileTimeWriter', ['time' => time()]);
-});
+// => MAILER QUEUE <=
+Route::get('users/store', 'UsersController@store');
+Route::resource('users', 'UsersController');
 
-Route::post('queue', function()
-{
-	return Queue::marshal();
-});
-
-class FileTimeWriter
-{
-	public function fire($job, $date)
-	{
-		File::append(app_path() . '/time.txt', $date['time'] . PHP_EOL);
-	}
-}
-
-
-// Route::get('/', array('as' => 'new_snippet', 'uses' => 'SnippetsController@create'));
-// Route::get('{num}', array('as' => 'snippet', 'uses' => 'SnippetsController@show'));
-// Route::get('{num}/fork', array('as' => 'fork_snippet', 'uses' => 'SnippetsController@fork'));
-// Route::post('/', array('uses' => 'SnippetsController@store'));
+// => QUEUES <=
+// Route::get('/', function()
+// {
+// 	Queue::push('FileTimeWriter', ['time' => time()]);
+// });
 //
+// Route::post('queue', function()
+// {
+// 	return Queue::marshal();
+// });
+//
+// class FileTimeWriter
+// {
+// 	public function fire($job, $date)
+// 	{
+// 		File::append(app_path() . '/time.txt', $date['time'] . PHP_EOL);
+// 	}
+// }
+
+
+Route::get('/', array('as' => 'new_snippet', 'uses' => 'SnippetsController@create'));
+Route::get('{num}', array('as' => 'snippet', 'uses' => 'SnippetsController@show'));
+Route::get('{num}/fork', array('as' => 'fork_snippet', 'uses' => 'SnippetsController@fork'));
+Route::post('/', array('uses' => 'SnippetsController@store'));
+
 
